0d265b5d828b9359a9f6fb705672b2f90ebbf216
Harish <harish@robogenix.ai>
2021-11-10 07:20:49 -0600

    Url to sentiment

--- sentimentAnalysis/__init__.py
+++ sentimentAnalysis/__init__.py
@@ -31,6 +31,15 @@ import nltk
 import os
 import json
 
+import urllib
+import pickle
+import speech_recognition as sr
+import os
+import json
+from moviepy.editor import *
+import requests
+
+r= sr.Recognizer()
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -40,18 +49,74 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except ValueError:
         pass
     
-    text =req_body['text']
-    blob = TextBlob(text)
-    polarity = blob.sentiment.polarity
-    print("polarity is")
-    print(polarity)
+    extractedURL =req_body['url']
     outputSentiment ="Nothing"
-    if(polarity < -0.1):
-        outputSentiment ="NEGATIVE, score =  " +str(polarity)
-    elif(-0.1<=polarity<=0.1):
-        outputSentiment ="NEUTRAL, score =  " +str(polarity)
-    elif(polarity>0.1):
-        outputSentiment ="POSITIVE, score =  " +str(polarity)
-    print(outputSentiment)
-    return func.HttpResponse(json.dumps(outputSentiment), mimetype='application/json' )
-    # return func.HttpResponse(json.dumps(polarity), mimetype='application/json' )
+    try:
+        sentiment,extractedText = urlToSentimentConverter(urls = extractedURL)
+        print("final sentiment "+str(sentiment))
+        return func.HttpResponse(json.dumps(sentiment), mimetype='application/json' )
+    except:
+        sentiment,extractedText = "error in urlToSentiment","error in urlToSentiment"
+    
+
+    
+def urlToSentimentConverter(urls):
+    def textToSentiment(text):
+        blob = TextBlob(text)
+        polarity = blob.sentiment.polarity
+        outputSentiment ="Nothing"
+        if(polarity < -0.1):
+            outputSentiment ="NEGATIVE, score =  " +str(polarity)
+        elif(-0.1<=polarity<=0.1):
+            outputSentiment ="NEUTRAL, score =  " +str(polarity)
+        elif(polarity>0.1):
+            outputSentiment ="POSITIVE, score =  " +str(polarity)
+        print("output sentiment = "+str(outputSentiment))
+        return(outputSentiment)
+    def startConvertion(path='/tmp/convertedAudio.wav', lang='en-IN'):
+        with sr.AudioFile(path) as source:
+            print('Fetching File')
+            audio_text = r.listen(source)
+            # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+            try:
+
+                # using google speech recognition
+                print('Converting audio transcripts into text ...')
+                text = r.recognize_google(audio_text)
+                print('Converted is '+str(text))
+                return text
+
+            except:
+                print('unable to convert audio to text')
+                return "error in text conversion"
+
+    print('Downloading video')
+    urllib.request.urlretrieve(urls, '/tmp/downloadedVideo.mp4')
+
+    print('Download completed')
+
+    print("reading video file")
+    # my_clip = mp.VideoFileClip(r'/tmp/downloadedVideo.mp4')
+
+    # my_clip.audio.write_audiofile(r"/tmp/my_result.wav")
+    # print('writting complete')
+    sound = AudioFileClip("/tmp/downloadedVideo.mp4")
+    print('writing audio file')
+    sound.write_audiofile("/tmp/convertedAudio.wav")
+    print('writting complete')
+
+    text = startConvertion(path='/tmp/convertedAudio.wav', lang='en-IN')
+    # text = 'that was very nice very very nice keep doing it its great'
+
+    if (text == "error in text conversion"):
+        interResult = ["Audio to text failed", ""]
+        print(interResult)
+        print(type(interResult))
+        return str(interResult[0]), text
+
+    else:
+        interResult = textToSentiment(text)
+        print(interResult)
+        print(type(interResult))
+        return str(interResult), text
+
